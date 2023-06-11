f7b7ed9cf5f008bf9734e7e42a90ee936eb1de1f
barzoid1 <128053888+barzoid1@users.noreply.github.com>
2023-06-11 23:51:39 +0100

    Add files via upload

---
+++ spideytoken/js/script.js
@@ -0,0 +1,12 @@
+new Swiper ("#swiper-1", {
+    loop:true,
+    pagination: {
+        el: "#swiper-1",
+        clicakble:true
+    },
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 1
+})
